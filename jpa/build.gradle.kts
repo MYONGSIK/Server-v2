e33cc3281b6d01d3937b0241fc5d44dd7df0e877
kyubeom <be_man@naver.com>
2024-08-18 21:36:42 +0900

    #1 jpa 모듈 분리

---
+++ jpa/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    idea
+}
+
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    //mysql
+    implementation("mysql:mysql-connector-java:8.0.33")
+}
